4c7b92adea91fc9b18441dbaa4a67a50d8f81144
ssskram <ssskram@ssskram.local>
2019-03-25 15:31:07 -0400

    arrows

--- routes/loginEvents.js
+++ routes/loginEvents.js
@@ -1,56 +1,45 @@
 const express = require('express')
 const router = express.Router()
-const checkToken = require('../token')
 const refreshToken = require('../refresh')
 const fetch = require('node-fetch')
 const dt = require('node-json-transform').DataTransform
 const models = require('../models/loginEvents')
 global.Headers = fetch.Headers
 
-router.get('/events',
-    async function (req, res) {
-        let events = []
-        const valid = (checkToken(req.token))
-        if (valid == true) {
-            const time = await getSpan(req.query.prevMinutes)
-            await callApi("https://graph.microsoft.com/beta/auditLogs/signIns?$filter=(status/errorCode eq 0) and createdDateTime ge " + time.from + "")
-            res.status(200).send(events)
-        } else res.status(403).end()
+router.get('/events', async (req, res) => {
+    let events = []
+    const time = await getSpan(req.query.prevMinutes)
+    await callApi("https://graph.microsoft.com/beta/auditLogs/signIns?$filter=(status/errorCode eq 0) and createdDateTime ge " + time.from + "")
+    res.status(200).send(events)
 
-        async function callApi(url) {
-            const response = await fetch(url, {
-                method: 'get',
-                headers: new Headers({
-                    'Authorization': 'Bearer ' + await refreshToken(),
-                    'Content-Type': 'application/json'
-                })
+    async function callApi(url) {
+        const response = await fetch(url, {
+            method: 'get',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + await refreshToken(),
+                'Content-Type': 'application/json'
             })
-            const data = await response.json()
-            await events.push(...dt(data, models.events).transform())
-            if (data["@odata.nextLink"]) {
-                await callApi(data["@odata.nextLink"])
-            } else return
-        }
+        })
+        const data = await response.json()
+        await events.push(...dt(data, models.events).transform())
+        if (data["@odata.nextLink"]) {
+            await callApi(data["@odata.nextLink"])
+        } else return
     }
-)
+})
 
-router.get('/singleEvent',
-    async function (req, res) {
-        const valid = (checkToken(req.token))
-        if (valid == true) {
-            fetch("https://graph.microsoft.com/beta/auditLogs/signIns/" + req.query.id, {
-                method: 'get',
-                headers: new Headers({
-                    'Authorization': 'Bearer ' + await refreshToken(),
-                    'Accept': 'application/json'
-                })
-            })
-                .then(res => res.json())
-                .then(data => res.status(200).send(data))
-                .catch(err => res.status(500).send(err))
-        } else res.status(403).end()
-    }
-)
+router.get('/singleEvent', async (req, res) => {
+    fetch("https://graph.microsoft.com/beta/auditLogs/signIns/" + req.query.id, {
+        method: 'get',
+        headers: new Headers({
+            'Authorization': 'Bearer ' + await refreshToken(),
+            'Accept': 'application/json'
+        })
+    })
+        .then(res => res.json())
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(500).send(err))
+})
 
 const getSpan = span => {
     const from = new Date()
